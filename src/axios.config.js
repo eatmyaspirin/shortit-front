f052bd31f05fac9476229afd99aaf8017b76de40
Ea <eatmyaspirin@gmail.com>
2024-02-15 19:21:34 +0530

    minor: fixes before deployement

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 axios.defaults.timeout = 1000;
 axios.defaults.withCredentials = true;
 
